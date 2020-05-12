f0dd87f03fe741e4a6247492c8bef4e64fd2c89d
Muhamar <amararrasyid@gmail.com>
2020-05-12 18:14:19 +0700

    Tugas 20 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,12 @@
+function mat() {
+    var isi = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable";
+
+    var kosong = null;
+
+    console.log(isi.match("are"));
+    console.log(kosong);
+    console.log(isi.match("words"));
+
+}
+
+mat();
